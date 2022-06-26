91e9e4eaed17a72a2d9b2048936eea4f870032a4
Asia84 <84asia@mail.ru>
2022-06-26 23:58:10 +0700

    решили первую задачу

---
+++ урок1/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+Напишите программу, которая на вход принимает число и выдает его квадрат(число умноженное на само себя)
+Пример:
+4 -> 16
+-3 -> 9
+-7 -> 49
+*/
+
+Console.WriteLine("Введите число для возведения в квадрат");
+int number = Convert.ToInt32(Console.ReadLine());
+int squareOfNumber = number * number;
+
+Console.WriteLine($"Квадрат числа {number} равен {squareOfNumber}");
