a9c9390d9051df4109e62d2ae8e7063a26ae73d2
Asia84 <84asia@mail.ru>
2022-06-27 01:12:05 +0700

    решили восьмую задачу

--- урок8/Program.cs
+++ урок8/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*  Задача 6: Напишите программу, которая на вход принимает число и 
+ *  выдаёт, является ли число чётным (делится ли оно на два без остатка).
+ *  4 -> да
+ *  3 -> нет
+ *  7 -> нет
+ */
+
+Console.WriteLine($"Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+string result = "Нечетное";
+
+int isEven = num % 2;
+if(isEven == 0) result = "Четное";
+
+Console.WriteLine(result);
